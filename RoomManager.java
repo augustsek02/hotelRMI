00a9962045978c19da3c96e117e20baf340bcd10
Ntaryebwa Mukama <26729028+HelloMukama@users.noreply.github.com>
2022-07-05 10:12:56 +0300

    first commit

---
+++ RoomManager.java
@@ -0,0 +1,18 @@
+package hotel;
+import java.rmi.*;
+
+
+public interface RoomManager extends Remote {
+
+   public int[] list() throws RemoteException; 
+
+   public boolean book(int type, String guest) throws RemoteException; 
+
+   public String[] guests() throws RemoteException;
+
+   public double[] revenue() throws RemoteException;
+ 
+   public int[] occupied() throws RemoteException; 
+
+
+}
